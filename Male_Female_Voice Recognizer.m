d6a47b5c4af76716cd6ae14e3d03ba2bbbe3426a
harshmittal2210 <harshmittal2210@gmail.com>
2018-12-16 12:43:02 +0530

    Algo for Recognizer

--- Male_Female_Voice Recognizer.m
+++ Male_Female_Voice Recognizer.m
@@ -2,3 +2,23 @@ clc;clear all;close all;
 [y ,Fs]=audioread('samples/m2.wav');
 frame=3500; 
 [b0,a0]=mybutter(350/(Fs/2));
+for i=1:length(y)/frame
+    x=y(1+(i-1)*frame:i*frame);
+    xin = abs(x);
+    xin=myfilter(b0,a0,xin);    
+    xin = xin-mean(xin);   
+    x_out(1+(i-1)*frame:i*frame,1)=xin;
+    x2=zeros(length(xin),1);
+    x2(1:length(x)-1)=xin(2:length(x));
+    zc=length(find((xin>0 & x2<0) | (xin<0 & x2>0)));
+    F0(i)=0.5*Fs*zc/length(x);
+    
+end
+Fx=mean(F0);
+fprintf('Estimated frequency is %3.2f Hz.\n',Fx);
+fprintf('Estimated frequency by in-built function is %3.2f Hz.\n',mean(freq(y)));
+if Fx>165
+    fprintf('Female Voice\n');
+else
+    fprintf('Male Voice\n');
+end
